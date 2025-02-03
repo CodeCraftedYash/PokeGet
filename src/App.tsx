87a6f6256e1c6c371d72086ba3327788cf03f9fb
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-02-03 15:53:54 +0530

    added footer and modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const AppContent: React.FC = () => {
   return (
     <div className="bg-black w-full min-h-screen flex flex-col justify-start items-center p-2 border-b-2 pt-4 relative">
       <div className="flex flex-col items-center justify-center gap-2 ">
-        <h1 className="text-white text-3xl font-bold border-b-2 p-2 border-b-blue-400">
+        <h1 className="text-white text-3xl font-bold border-b-2 p-2 border-b-blue-400 ">
           Get Your Pokemon
         </h1>
         <BirthdayFormComponent />
@@ -33,7 +33,7 @@ const AppContent: React.FC = () => {
           <PokemonDisplay />
         </div>
         <button
-          className="text-white bg-blue-500 p-1 text-sm rounded-xl border-2 absolute bottom-64 "
+          className="text-white bg-blue-500 p-1 text-sm rounded-xl border-2 absolute bottom-40 "
           onClick={() => {
             setDisplay(!display); // This toggles the display state
           }}
@@ -41,6 +41,9 @@ const AppContent: React.FC = () => {
           Retry
         </button>
       </div>
+      <footer className="w-full bg-gray-900 text-white text-center py-4 absolute bottom-0">
+      <p className="text-sm">© {new Date().getFullYear()} Yash Kr. Mishra. All rights reserved.</p>
+    </footer>
     </div>
   );
 };
